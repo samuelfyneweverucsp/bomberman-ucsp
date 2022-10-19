7b1530d7b82e365cca3417b8b8cb173bf4f085b5
sbf5calvin <112088637+samuelfyneweverucsp@users.noreply.github.com>
2022-10-19 07:55:30 -0500

    escenario

---
+++ Escenario.h
@@ -0,0 +1,18 @@
+#ifndef __ESCENARIO_H__
+#define __ESCENARIO_H__
+
+int filas = 15;
+int columnas = 17;
+
+class CEscenario {
+    public:
+        CEscenario(){};
+        ~CEscenario(){};
+    private:
+        int **matriz;
+
+};
+
+#endif 
+
+
---
+++ Juego.cpp
@@ -0,0 +1,10 @@
+#include "juego.h"
+
+// using namespace JuegoBomberman;
+using namespace std;
+
+void main () {
+    //Application::EnableVisualStyles;
+    //Application::Run(gcnew Juego());
+    
+}
