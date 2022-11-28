1de389cea440e4522fc16f751d1687e8fc7dec0e
ValenSaZu <111878177+ValenSaZu@users.noreply.github.com>
2022-11-27 21:31:05 -0500

    Add files via upload

--- Bomberman/Juego.h
+++ Bomberman/Juego.h
@@ -11,7 +11,6 @@ namespace Bomberman {
 	using namespace System::Drawing;
 	using namespace System::Media;
 
-
 	/// <summary>
 	/// Resumen de Juego
 	/// </summary>
@@ -26,8 +25,8 @@ namespace Bomberman {
 		Bitmap^ bmpJugador = gcnew Bitmap("Imagenes\\Jugador.png");
 		Bitmap^ bmpBomba = gcnew Bitmap("Imagenes\\bomba.png");
 		Bitmap^ bmpExplosion = gcnew Bitmap("Imagenes\\explosion.png");
-		//Bitmap^ bmpEnemigo = gcnew Bitmap("Imagenes\\bmpEnemigo.png");
-		//Bitmap^ bmpMejoras = gcnew Bitmap("Imagenes\\bmpMejoras.png");
+		Bitmap^ bmpMejoras = gcnew Bitmap("Imagenes\\bmpMejoras.png");
+	   //Bitmap^ bmpEnemigo = gcnew Bitmap("Imagenes\\bmpEnemigo.png");
 	public:
 		Juego(void)
 		{
@@ -36,6 +35,7 @@ namespace Bomberman {
 			bmpExplosion->MakeTransparent(bmpExplosion->GetPixel(0,0));
 			//bmpEnemigo->MakeTransparent(bmpEnemigo->GetPixel(0,0));
 			InitializeComponent();
+			oControlador = new CControlador();
 			//
 			//TODO: agregar c�digo de constructor aqu�
 			//
@@ -52,6 +52,9 @@ namespace Bomberman {
 				delete components;
 			}
 		}
+private: System::Windows::Forms::Label^ lblNivel;
+private: System::Windows::Forms::ProgressBar^ pbCarga;
+private: System::Windows::Forms::Timer^ trCarga;
 	private: System::Windows::Forms::Timer^ timer1;
 	// protected:
 	// private: 
@@ -72,36 +75,68 @@ namespace Bomberman {
 		{
 			this->components = (gcnew System::ComponentModel::Container());
 			this->timer1 = (gcnew System::Windows::Forms::Timer(this->components));
+			this->lblNivel = (gcnew System::Windows::Forms::Label());
+			this->pbCarga = (gcnew System::Windows::Forms::ProgressBar());
+			this->trCarga = (gcnew System::Windows::Forms::Timer(this->components));
 			this->SuspendLayout();
 			// 
 			// timer1
 			// 
-			this->timer1->Enabled = true;
 			this->timer1->Tick += gcnew System::EventHandler(this, &Juego::timer1_Tick);
+			// 
+			// lblNivel
+			// 
+			this->lblNivel->AutoSize = true;
+			this->lblNivel->BackColor = System::Drawing::Color::Transparent;
+			this->lblNivel->Font = (gcnew System::Drawing::Font(L"Trebuchet MS", 20.25F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->lblNivel->ForeColor = System::Drawing::SystemColors::ButtonFace;
+			this->lblNivel->Location = System::Drawing::Point(212, 237);
+			this->lblNivel->Name = L"lblNivel";
+			this->lblNivel->Size = System::Drawing::Size(89, 35);
+			this->lblNivel->TabIndex = 0;
+			this->lblNivel->Text = L"Nivel:";
+			// 
+			// pbCarga
+			// 
+			this->pbCarga->Location = System::Drawing::Point(188, 300);
+			this->pbCarga->Name = L"pbCarga";
+			this->pbCarga->Size = System::Drawing::Size(161, 23);
+			this->pbCarga->TabIndex = 1;
+			// 
+			// trCarga
+			// 
+			this->trCarga->Enabled = true;
+			this->trCarga->Interval = 2500;
+			this->trCarga->Tick += gcnew System::EventHandler(this, &Juego::trCarga_Tick);
 			// 
 			// Juego
 			// 
 			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
-			this->ClientSize = System::Drawing::Size(850, 750);
+			this->BackColor = System::Drawing::SystemColors::InactiveCaptionText;
+			this->ClientSize = System::Drawing::Size(584, 561);
+			this->Controls->Add(this->pbCarga);
+			this->Controls->Add(this->lblNivel);
 			this->Name = L"Juego";
 			this->Text = L"Juego";
 			this->Load += gcnew System::EventHandler(this, &Juego::Juego_Load);
 			this->KeyDown += gcnew System::Windows::Forms::KeyEventHandler(this, &Juego::MantenerTecla);
 			this->KeyUp += gcnew System::Windows::Forms::KeyEventHandler(this, &Juego::UltimaTeclaPresionada);
 			this->ResumeLayout(false);
+			this->PerformLayout();
 
 		}
 #pragma endregion
-		void MusicaNivel() {
+		void MusicNivel() {
 			MusicaN = gcnew SoundPlayer("Audio\\ModoSolitario.wav");
 		}
 	private: System::Void timer1_Tick(System::Object^ sender, System::EventArgs^ e) {
 		Graphics^ g = this->CreateGraphics();
 		BufferedGraphicsContext^ espacio = BufferedGraphicsManager::Current;
 		BufferedGraphics^ buffer = espacio->Allocate(g, this->ClientRectangle);
-		MusicaNivel();
-		oControlador->dibujar(buffer->Graphics, bmpSuelo, bmpSolido, bmpBomba, bmpExplosion, bmpDestruible, bmpJugador);// , bmpMejoras, bmpEnemigo);
+		MusicNivel();
+		oControlador->dibujar(buffer->Graphics, bmpSuelo, bmpSolido, bmpBomba, bmpExplosion, bmpDestruible, bmpJugador, bmpMejoras);// , bmpMejoras, bmpEnemigo);
 		buffer->Render(g);
 		delete buffer, espacio, g;
 	}
@@ -140,22 +175,21 @@ namespace Bomberman {
 			}
 		}
 
-/*	private: System::Void trCarga_Tick(System::Object^ sender, System::EventArgs^ e) {
-		lblNivel->Text = "Nivel: " + oControlador->getNivel();
-		pbCarga->Increment(10);
-		if(trCarga->Interval == 2500 && oControlador->getoArrEnemigos()->getarregloEnemigos().size() < oControlador->getNivel()) { 
-			oControlador->crear_enemigos_y_mejoras();
-		}
-		else {
-			trCarga->Enabled = false;
-			timer1->Enabled = true;
-			MusicaNivel();
-			lblNivel->Visible = false;
-			lblNivel->Enabled = false;
-			pbCarga->Visible = false;
-			pbCarga->Enabled = false;
-		}
+private: System::Void trCarga_Tick(System::Object^ sender, System::EventArgs^ e) {
+	lblNivel->Text = "Nivel: " + oControlador->getNivel();
+	pbCarga->Increment(10);
+	if (trCarga->Interval == 2500 && oControlador->getoArrMejoras()->getvector_mejoras().size()<oControlador->getNivel()) {
+		oControlador->crear_enemigos_y_mejoras();
 	}
-	*/
+	else {
+		trCarga->Enabled = false;
+		timer1->Enabled = true;
+
+		lblNivel->Visible = false;
+		lblNivel->Enabled = false;
+		pbCarga->Visible = false;
+		pbCarga->Enabled = false;
+	}
+}
 };
 }
