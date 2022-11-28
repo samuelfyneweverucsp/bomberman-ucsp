d936940cc73209c696de0ece7722ba43b5a53409
ValenSaZu <111878177+ValenSaZu@users.noreply.github.com>
2022-11-27 21:24:04 -0500

    Add files via upload

--- Bomberman/Controlador.h
+++ Bomberman/Controlador.h
@@ -3,7 +3,7 @@
 #include "Escenario.h"//con el nombre con que lo hayan puesto
 #include "Jugador.h"
 #include "ArrBombas.h"
-//#include "ArrMejoras.h"
+#include "ArrMejoras.h"
 //#include "ArrEnemigos.h"
 
 class CControlador
@@ -12,16 +12,17 @@ private:
 	CEscenario* oEscenario;
 	CJugador* oJugador;
 	CArrBombas* oArrBombas;
-	//CArrMejoras* oArrMejoras;
+	CArrMejoras* oArrMejoras;
 	//CArrEnemigos* oArrEnemigos;
-	int nivel = 1;
+	int nivel;
 
 public:
 	CControlador(){ // constructor
 		oEscenario = new CEscenario;
 		oJugador = new CJugador(50, 50); // declarar jugador e inicializar en la posicion (50; 50)
 		oArrBombas = new CArrBombas();
-		//oArrMejoras = new CArrMejoras();
+		oArrMejoras = new CArrMejoras();
+		nivel = 1;
 		//oArrEnemigos = new CArrEnemigos();
 		}
 	~CControlador(){} // destructor
@@ -66,24 +67,29 @@ public:
 	}
 
 	void dibujar(Graphics ^ g, Bitmap ^ bmpBase, Bitmap ^ bmpSolido, Bitmap ^ bmpBomba, 
-				Bitmap ^ bmpExplosion, Bitmap ^ bmpDestruible, Bitmap ^ bmpJugador)//,Bitmap^ bmpMejoras)//, Bitmap ^ bmpEnemigo)
+				Bitmap ^ bmpExplosion, Bitmap ^ bmpDestruible, Bitmap ^ bmpJugador,Bitmap^ bmpMejoras)//, Bitmap ^ bmpEnemigo)
 	{
 		oEscenario->PintarBase(g, bmpBase);
+		//mejoras
 		oEscenario->PintarMatriz(g, bmpSolido, bmpDestruible);
+		oArrMejoras->dibujar(g, bmpMejoras, oEscenario->getMatriz());
 		oJugador->moverJugador(g, bmpJugador, oEscenario->getMatriz()); 
 		oArrBombas->dibujar_una_bomba(g, bmpBomba, bmpExplosion, oJugador->getX(), oJugador->getY(), oEscenario->getMatriz());
 		//oArrEnemigos->dibujar(g, bmpEnemigo, oEscenario->getMatriz());
 	} 
-
+	void crear_enemigos_y_mejoras() {
+		oArrMejoras->crearMejoras();
+	}
 	CJugador* getoJugador () {
 		return oJugador;
 	}
-
-	/*CArrMejoras* getoArrMejoras() {
+	CArrMejoras* getoArrMejoras() {
 		return oArrMejoras;
 	}
-
-	CArrEnemigos* getoArrEnemigos() {
+	int getNivel() {
+		return nivel;
+	}
+	/*CArrEnemigos* getoArrEnemigos() {
 		return oArrEnemigos;
 	}*/
 };
